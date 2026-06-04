675ec0383ff62b13062270c2b00e3b303ab403aa
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-06-04 20:00:42 +0530

    feat: auth register, login and JWT middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const app = express();
 const pool = require("./db");
+const authRoutes = require("./routes/auth");
+
+require("dotenv").config();
+
+const app = express();
+
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Media Locker API is running" });
+});
+
 pool.query("SELECT NOW()", (err, res) => {
   if (err) console.error("DB connection error", err);
   else console.log("DB connected at", res.rows[0].now);
 });
-app.get("/", (req, res) => {
-  res.json({ message: "Media locker API running" });
-});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
