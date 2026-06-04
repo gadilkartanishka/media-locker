2c225604813055e43f2261a38280f1c245a4425b
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-06-04 19:10:44 +0530

    feat: connect express to postgresql

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const pool = require("./db");
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) console.error("DB connection error", err);
+  else console.log("DB connected at", res.rows[0].now);
+});
 app.get("/", (req, res) => {
   res.json({ message: "Media locker API running" });
 });
